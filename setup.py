feb362ab29067f70aa6447a5c4a3b4de51c3bb2e
Nuno Teixeira <teixas@gmail.com>
2012-03-11 18:41:51 +0000

    Setup testing

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
+import subprocess
+import sys
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup, Command
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -10,9 +12,32 @@ try:
 except IOError:
     README = CHANGES = ''
 
+install_requires = [
+    'Kotti',
+    ]
+
+test_requires = [
+    'pytest',
+    'pytest-cov'
+]
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        script = os.path.join(os.path.dirname(sys.executable), 'py.test')
+        errno = subprocess.call([sys.executable, script])
+        raise SystemExit(errno)
 
 setup(name='kotti_solr',
-      version='0.1a',
+      version='0.1',
       description="Solr integration for Kotti",
       long_description='\n\n'.join([README, CHANGES]),
       classifiers=[
@@ -31,7 +56,6 @@ setup(name='kotti_solr',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-        'Kotti',
-      ],
+      install_requires=install_requires + test_requires,
+      cmdclass={'test': PyTest},
       )
