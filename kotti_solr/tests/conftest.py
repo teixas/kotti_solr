9dd64533ce76a1d2b55da20c83bff80519aa352c
Andreas Zeidler <az@zitc.de>
2012-05-23 19:36:44 +0200

    let the `solr` test funcarg return the `SolrInterface` object so it actually has a use (beside the side-effect of running Solr)

--- kotti_solr/tests/conftest.py
+++ kotti_solr/tests/conftest.py
@@ -1,4 +1,5 @@
 from mr.laforge import up, waitforports, shutdown
+from sunburnt import SolrInterface
 
 
 def pytest_funcarg__solr(request):
@@ -6,4 +7,5 @@ def pytest_funcarg__solr(request):
         up('solr')
         waitforports(8983)      # wait for Solr to start up
         request.addfinalizer(shutdown)
+        return SolrInterface('http://localhost:8983/solr')
     return request.cached_setup(setup=setup, scope='session')
--- kotti_solr/tests/test_solr.py
+++ kotti_solr/tests/test_solr.py
@@ -1,6 +1,4 @@
-from sunburnt import SolrInterface
 
 
 def test_access_solr_schema(solr):
-    solr = SolrInterface('http://localhost:8983/solr')
     assert 'title' in solr.schema.fields
