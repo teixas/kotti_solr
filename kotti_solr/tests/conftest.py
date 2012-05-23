1ed9c1155addd43ee553b4e0e2ac7ffbeb9be22b
Andreas Zeidler <az@zitc.de>
2012-05-23 19:47:08 +0200

    add app settings for testing (including `kotti_solr.solr_url`)

--- kotti_solr/tests/conftest.py
+++ kotti_solr/tests/conftest.py
@@ -2,10 +2,19 @@ from mr.laforge import up, waitforports, shutdown
 from sunburnt import SolrInterface
 
 
+settings = {
+    'kotti.site_title': 'My Kotti Solr-ized site',
+    'kotti.secret': 'qwerty',
+    'pyramid.includes': 'kotti_solr',
+    'sqlalchemy.url': 'sqlite:///%(here)s/Kotti.db',
+    'kotti_solr.solr_url': 'http://localhost:8983/solr',
+}
+
+
 def pytest_funcarg__solr(request):
     def setup():
         up('solr')
         waitforports(8983)      # wait for Solr to start up
         request.addfinalizer(shutdown)
-        return SolrInterface('http://localhost:8983/solr')
+        return SolrInterface(settings['kotti_solr.solr_url'])
     return request.cached_setup(setup=setup, scope='session')
