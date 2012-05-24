8b39e258ef734bd404ef573a5d4aa681cdece509
Andreas Zeidler <az@zitc.de>
2012-05-24 14:36:03 +0200

    objects set up by populators don't have a request associated yet, so we cannot make `path` required

--- kotti_solr/events.py
+++ kotti_solr/events.py
@@ -5,12 +5,14 @@ def add_document_handler(event):
     si = get_solr()
     doc = event.object
     request = event.request
-    si.add(dict(
+    data = dict(
             id=u'%s-%s' % (doc.type, doc.id),
             title=doc.title,
             description=doc.description,
             text=doc.body,
             last_modified=doc.modification_date,
-            path=request.resource_path(doc),
-            ))
+            )
+    if request is not None:
+        data['path'] = request.resource_path(doc)
+    si.add(data)
     si.optimize()
